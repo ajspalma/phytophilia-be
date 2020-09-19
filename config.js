09cb95a9e42f36eb597ec4b143a33826f7f3fe58
ajspalma <palma.ashleyjs@gmail.com>
2020-09-19 12:32:01 +0800

    Moved dotenv package to devDependencies to not use it as a runtime dependent package

--- config.js
+++ config.js
@@ -1,8 +1,3 @@
-const dotenv = require('dotenv');
-
-// parse .env file to load custom environment variables 
-dotenv.config();
-
 module.exports = {
     port: process.env.PORT || '3000',
     trefleKey: process.env.TREFLE_API_KEY || ''
