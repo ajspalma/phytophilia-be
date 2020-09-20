2464f9a5bda2f1a07fc6a6b36a71a14c7866a54d
ajspalma <palma.ashleyjs@gmail.com>
2020-09-20 12:09:27 +0800

    Added CORS package

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const { port, trefleKey } = require('./config')
 const express = require('express');
+const cors = require('cors');
 const fetch = require('node-fetch');
 
 const app = express();
 
+// Run cors through the express middleware to allow CORS
+app.use(cors());
+
 app.get('/', (req, res) => {
-    fetch(`https://trefle.io/api/v1/plants?token=${trefleKey}`)
+    fetch(`https://trefle.io/api/v1/plants?token=${trefleKey}&order[common_name]=asc`)
         .then(response => response.json())
         .then(data => {
             res.send(data);
